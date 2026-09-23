dacede7f374e22a48ca8e3af807cc504c62f30e4
KuLiPai <yun.svip@gmail.com>
2026-09-23 20:05:26 +0800

    fix(hook): 解决项目报错行数出错问题

--- app/src/main/java/com/kulipai/luahook/hook/entry/MainHook.kt
+++ app/src/main/java/com/kulipai/luahook/hook/entry/MainHook.kt
@@ -149,11 +149,13 @@ class MainHook : IXposedHookZygoteInit, IXposedHookLoadPackage {
                                             return oldLoadDex(path)
                                         end
                                     end
-                                """.trimIndent() + "\n" + rawScript
+                                """.trimIndent()
+
 
                             val globals = LuaHookEngine.load( this, projectName)
-                            registerExtensions(globals, projectName)
                             LuaHookEngine.run(globals,wrappedScript)
+                            registerExtensions(globals, projectName)
+                            LuaHookEngine.run(globals,rawScript)
                         }
                     } catch (e: Exception) {
                         "${lpparam.packageName}:[Project:$projectName]:${e.message}".e()
